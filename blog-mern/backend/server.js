c3522edb04172f820c3b06ff74fda6d4be98970f
vigneshmudgonda <124809262+vigneshmudgonda@users.noreply.github.com>
2025-08-17 14:48:19 +0530

    Update server.js

--- blog-mern/backend/server.js
+++ blog-mern/backend/server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
   // Catch-all route for React Router
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
